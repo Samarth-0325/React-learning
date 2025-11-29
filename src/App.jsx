cdb5f2c8de788f652f6aa1dfc687ddea94717210
Samarth Tiwari <samarthtiwari325@gmail.com>
2025-11-30 01:20:41 +0530

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div id="papa">
+      <h1>Hello beta</h1>
+      </div>
+
+      <div>
+        <h2>Ha papa</h2>
+      </div>
+      
+      </>
+  )
+ 
+
+  
+    
+  
+}
+
+export default App
